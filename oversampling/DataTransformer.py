4d10fa5f6b9696158584f054d4162feafe6c4599
boyfws <minkin.d.d@gmail.com>
2025-05-17 17:36:58 +0300

    Temporal save

--- oversampling/DataTransformer.py
+++ oversampling/DataTransformer.py
@@ -3,219 +3,214 @@ import pandas as pd
 import torch
 from pandas.api.types import is_bool_dtype, is_numeric_dtype
 from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder
+from collections import OrderedDict
 
 
 class DataTransformer:
     def __init__(self):
         self.final_scaler = MinMaxScaler(feature_range=(-1, 1))
-        self.encoders = {}
-        self.min_max = {}
-        self.types = {}
-        self.apply_round = {}
-        self.masked_cols = []
-        self.columns = []
 
+        self.columns_data = {}
+        self.initial_columns_order = []
         self.fitted = False
 
     @staticmethod
-    def _get_unique_vals(col: pd.Series) -> list:
-        unique = col.unique()
-        nans = pd.isna(unique)
-
-        uniq_list = list(unique[~nans])
-        if any(nans):
-            uniq_list.append(None)
-
-        return uniq_list
-
-    @staticmethod
-    def _check_df(df: pd.DataFrame) -> pd.DataFrame:
+    def _check_df(df: pd.DataFrame) -> None:
         if df.empty:
             raise RuntimeError("DataFrame is empty")
 
         if df.select_dtypes(include="number").map(np.isinf).any(axis=None):
             raise RuntimeError("DataFrame is infinite")
 
+    def _prepare_indices(self) -> None:
+        self._softmax_idx = {
+            tuple(self.columns_data[el]["one_hot"]["one_hot_idx"]): el for el in self.columns_data
+            if len(self.columns_data[el]["one_hot"]["one_hot_idx"]) != 0
+        }
+
+        self._masked_idx = {
+            (
+                self.columns_data[el]["col_idx"],
+                self.columns_data[el]["mask_idx"]
+            ): el for el in self.columns_data
+            if self.columns_data[el]["mask_idx"] is not None
+        }
+
+        self._ordinal_idx = {
+            self.columns_data[el]["col_idx"]: el
+            for el in self.columns_data
+            if self.columns_data[el]["mask_idx"] is None
+            and self.columns_data[el]["col_idx"] is not None
+        }
+        self._ordinal_idx_list = list(self._ordinal_idx.keys())
+
     def fit(self, df: pd.DataFrame) -> None:
         self._check_df(df)
+        self.initial_columns_order = df.columns.tolist()
 
-        df = df.copy()
+        new_df = OrderedDict()
+        i = 0
 
         for el in df.columns:
-            self.types[el] = df[el].dtype
-            self.apply_round[el] = False
+            self.columns_data[el] = {
+                "dtype": df[el].dtype,
+                "apply_round": False,
+                "col_idx": None,  # Columns id
+                "mask_idx": None,  # Mask id
+                "one_hot": {
+                    "categories": [],  # List of categories
+                    "one_hot_idx": [],  # Start and end idx
+                }
+
+            }
 
             if is_numeric_dtype(df[el]) or is_bool_dtype(df[el]):
-
                 mask = df[el].isna()
+
                 if mask.any():
-                    df[f"{el}_mask"] = mask
-                    self.masked_cols.append(el)
+                    new_df[el] = df[el].fillna(0, inplace=False)
+                    new_df[f"{el}_mask"] = mask
+                    self.columns_data[el]["col_idx"] = i
+                    self.columns_data[el]["mask_idx"] = i + 1
+                    i += 2
+                else:
+                    new_df[el] = df[el]
+                    self.columns_data[el]["col_idx"] = i
+                    i += 1
 
                 if (df[el][~mask].round() == df[el][~mask]).fillna(False).all():
-                    self.apply_round[el] = True
+                    self.columns_data[el]["apply_round"] = True
 
             else:
-                self.encoders[el] = OrdinalEncoder(
-                    categories=[self._get_unique_vals(df[el])], encoded_missing_value=-1
-                )
-                transformed = self.encoders[el].fit_transform(
-                    df[el].where(pd.notnull(df[el]), None).to_numpy().reshape(-1, 1)
-                )
+                cat_df = pd.get_dummies(df[el], dummy_na=True)
+                self.columns_data[el]["one_hot"]["categories"] = cat_df.columns.to_list()
+                self.columns_data[el]["one_hot"]["one_hot_idx"] = [i, i + cat_df.shape[1] - 1]
+                i += cat_df.shape[1]
 
-                df[el] = transformed
-                self.min_max[el] = [transformed.min(), transformed.max()]
+                for col in cat_df.columns:
+                    new_df[f"{el}_{col}"] = cat_df[col]
 
-        self.columns = df.columns
+        self.final_scaler.fit(pd.DataFrame(new_df).to_numpy())
 
-        self.final_scaler.fit(df.to_numpy())
+        self.final_size = len(new_df)
+        self._prepare_indices()
 
         self.fitted = True
 
     def transform(self, df: pd.DataFrame) -> torch.Tensor:
         self._check_df(df)
-        df = df.copy()
 
-        for el in df.columns:
-            if is_numeric_dtype(df[el]) or is_bool_dtype(df[el]):
+        new_df = OrderedDict()
 
-                if el in self.masked_cols:
-                    mask = df[el].isna()
-                    df[f"{el}_mask"] = mask
+        for el in self.initial_columns_order:
+            if self.columns_data[el]["col_idx"] is not None:
+                new_df[el] = df[el]
 
-                    df[el] = df[el].fillna(0)
+                if self.columns_data[el]["mask_idx"] is not None:
+                    new_df[el] = df[el].fillna(0, inplace=False)
+                    new_df[f"{el}_mask"] = df[el].isna()
+                else:
+                    new_df[el] = df[el]
 
             else:
-                df[el] = df[el].where(pd.notnull(df[el]), None)
-                transformed = self.encoders[el].transform(
-                    df[el].to_numpy().reshape(-1, 1)
-                )
+                cat_df = pd.get_dummies(df[el], dummy_na=True)
+                cat_df_cols_set = set(cat_df.columns)
 
-                df[el] = transformed
+                extra = cat_df_cols_set - set(self.columns_data[el]["one_hot"]["categories"])
 
-        scaled = self.final_scaler.transform(df.to_numpy())
+                if extra:
+                    raise ValueError(f"Unexpected categories: {extra}")
 
-        return torch.Tensor(scaled.astype(np.float32))
+                for col in self.columns_data[el]["one_hot"]["categories"]:
+                    if col not in cat_df_cols_set:
+                        new_df[f"{el}_{col}"] = [False] * df.shape[0]
+                    else:
+                        new_df[f"{el}_{col}"] = cat_df[col]
+
+        return torch.tensor(
+            self.final_scaler.transform(
+                pd.DataFrame(new_df).to_numpy()
+            ),
+            dtype=torch.float32,
+        )
 
     def fit_transform(self, df: pd.DataFrame) -> torch.Tensor:
         self.fit(df)
         return self.transform(df)
 
+    def calculate_mse_vae(
+            self,
+            reconstructed: torch.Tensor,
+            true: torch.Tensor
+    ) -> torch.Tensor:
+
+        loss = torch.nn.functional.mse_loss(
+            reconstructed[:, self._ordinal_idx_list],
+            true[:, self._ordinal_idx_list],
+            reduction="sum"
+        )
+        for el in self._softmax_idx:
+            loss += torch.nn.functional.mse_loss(
+                torch.nn.functional.softmax(reconstructed[:, el[0]: el[1] + 1], dim=1),
+                true[:, el[0]: el[1] + 1],
+                reduction="sum"
+            )
+
+        for el in self._masked_idx:
+            loss += (
+                    torch.nn.functional.mse_loss(
+                        reconstructed[:, el[0]],
+                        true[:, el[0]],
+                        reduction="none"
+                    ) * (
+                        1 - true[:, el[1]]
+                    )
+            ).sum()
+
+            loss += torch.nn.functional.mse_loss(
+                reconstructed[:, el[1]],
+                true[:, el[1]],
+                reduction="sum"
+            )
+
+        return loss
+
     def inverse_transform(self, tensor: torch.Tensor) -> pd.DataFrame:
         if not self.fitted:
             raise RuntimeError(
-                "fit_transform method must be called before inverse_transform"
+                "fit method must be called before inverse_transform"
             )
 
         tensor_unscaled = self.final_scaler.inverse_transform(tensor)
+        new_df = {value: tensor_unscaled[:, key] for key, value in self._ordinal_idx.items()}
 
-        df = pd.DataFrame(tensor_unscaled, columns=self.columns)
-        for el in df.columns:
+        for key, value in self._softmax_idx.items():
+            new_df[value] = [
+                self.columns_data["one_hot"]["categories"][i]
+                for i in np.argmax(tensor_unscaled[:, key[0]: key[1] + 1].numpy(), axis=1)
+            ]
 
-            if el in self.encoders:
-                df[el] = (
-                    self.encoders[el]
-                    .inverse_transform(
-                        df[el]
-                        .clip(lower=self.min_max[el][0], upper=self.min_max[el][1])
-                        .round()
-                        .astype(int)
-                        .to_numpy()
-                        .reshape(-1, 1)
-                    )
-                    .reshape(-1)
-                )
+        for key, value in self._masked_idx.items():
+            ser_tens = pd.Series(tensor_unscaled[:, key[0]].numpy())
+            mask = tensor_unscaled[:, key[1]].numpy() >= 0.5
+            ser_tens[mask] = np.nan
+            new_df[value] = ser_tens
 
-            if el.endswith("_mask"):
-                col = el[:-5]
+        df = pd.DataFrame(new_df)
+        for el in self.columns_data:
 
-                mask = df[el] >= 0.5
-
-                df.loc[mask, col] = np.nan
-
-                df = df.drop(el, axis=1)
-
-        for el in self.types:
-
-            if self.apply_round[el]:
+            if self.columns_data[el]["apply_round"]:
                 df[el] = df[el].round()
 
-            df[el] = df[el].astype(self.types[el])
+            df[el] = df[el].astype(self.columns_data[el]["dtype"])
+
 
         return df
 
+
     def get_params(self) -> dict:
-        if not self.fitted:
-            raise RuntimeError("fit_transform method must be called before get_params")
-        encoders = {}
-        for col, enc in self.encoders.items():
-            mi = None
-            if hasattr(enc, "_missing_indices"):
-                mi = enc._missing_indices
-
-            else:
-                cats = enc.categories_[0]
-                mask = pd.isna(cats)
-                idx = int(np.where(mask)[0][0]) if mask.any() else None
-                mi = [idx]
-
-            encoders[col] = {
-                "categories": enc.categories_[0].tolist(),
-                "encoded_missing_value": enc.encoded_missing_value,
-                "missing_indices": mi,
-                "_infrequent_enabled": getattr(enc, "_infrequent_enabled", False),
-                "_n_features_outs": getattr(enc, "_n_features_outs", 1),
-            }
-
-        return {
-            "final_scaler": {
-                "scale_": self.final_scaler.scale_.tolist(),
-                "min_": self.final_scaler.min_.tolist(),
-                "data_min_": self.final_scaler.data_min_.tolist(),
-                "data_max_": self.final_scaler.data_max_.tolist(),
-                "feature_range": self.final_scaler.feature_range,
-            },
-            "encoders": encoders,
-            "min_max": self.min_max,
-            "types": {col: str(dtype) for col, dtype in self.types.items()},
-            "columns": self.columns.tolist(),
-            "apply_round": self.apply_round,
-            "masked_cols": self.masked_cols,
-        }
+        pass
 
     @classmethod
     def save_params(cls, params: dict) -> "DataTransformer":
-        self = cls.__new__(cls)
-
-        fs = params["final_scaler"]
-        scaler = MinMaxScaler(feature_range=tuple(fs["feature_range"]))
-        scaler.scale_ = np.array(fs["scale_"])
-        scaler.min_ = np.array(fs["min_"])
-        scaler.data_min_ = np.array(fs["data_min_"])
-        scaler.data_max_ = np.array(fs["data_max_"])
-        scaler.data_range_ = scaler.data_max_ - scaler.data_min_
-        self.final_scaler = scaler
-
-        # encoders
-        self.encoders = {}
-        for col, st in params["encoders"].items():
-            enc = OrdinalEncoder(
-                categories=[st["categories"]],
-                encoded_missing_value=st["encoded_missing_value"],
-            )
-            enc.categories_ = [np.array(st["categories"])]
-            enc._missing_indices = st["missing_indices"]
-            enc._infrequent_enabled = st["_infrequent_enabled"]
-            enc._n_features_outs = st["_n_features_outs"]
-            self.encoders[col] = enc
-
-        # other
-        self.min_max = params["min_max"]
-        self.types = {col: np.dtype(dt) for col, dt in params["types"].items()}
-        self.columns = pd.Index(params["columns"])
-
-        self.apply_round = params["apply_round"]
-        self.masked_cols = params["masked_cols"]
-        self.fitted = True
-
-        return self
+        pass

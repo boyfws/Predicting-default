afad30bbdb01449242f34c6016d549c560cbcbe5
boyfws <minkin.d.d@gmail.com>
2025-05-17 18:15:08 +0300

    Temporal save 2

--- oversampling/DataTransformer.py
+++ oversampling/DataTransformer.py
@@ -2,17 +2,20 @@ import numpy as np
 import pandas as pd
 import torch
 from pandas.api.types import is_bool_dtype, is_numeric_dtype
-from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder
+from sklearn.preprocessing import MinMaxScaler
 from collections import OrderedDict
+
+MISSING_VALUE = "__MISSING__"
 
 
 class DataTransformer:
     def __init__(self):
-        self.final_scaler = MinMaxScaler(feature_range=(-1, 1))
+        self._final_scaler = MinMaxScaler(feature_range=(-1, 1))
 
-        self.columns_data = {}
-        self.initial_columns_order = []
-        self.fitted = False
+        self._columns_data = {}
+        self._initial_columns_order = []
+        self._fitted = False
+        self._final_size = None
 
     @staticmethod
     def _check_df(df: pd.DataFrame) -> None:
@@ -24,35 +27,35 @@ class DataTransformer:
 
     def _prepare_indices(self) -> None:
         self._softmax_idx = {
-            tuple(self.columns_data[el]["one_hot"]["one_hot_idx"]): el for el in self.columns_data
-            if len(self.columns_data[el]["one_hot"]["one_hot_idx"]) != 0
+            tuple(self._columns_data[el]["one_hot"]["one_hot_idx"]): el for el in self._columns_data
+            if len(self._columns_data[el]["one_hot"]["one_hot_idx"]) != 0
         }
 
         self._masked_idx = {
             (
-                self.columns_data[el]["col_idx"],
-                self.columns_data[el]["mask_idx"]
-            ): el for el in self.columns_data
-            if self.columns_data[el]["mask_idx"] is not None
+                self._columns_data[el]["col_idx"],
+                self._columns_data[el]["mask_idx"]
+            ): el for el in self._columns_data
+            if self._columns_data[el]["mask_idx"] is not None
         }
 
         self._ordinal_idx = {
-            self.columns_data[el]["col_idx"]: el
-            for el in self.columns_data
-            if self.columns_data[el]["mask_idx"] is None
-            and self.columns_data[el]["col_idx"] is not None
+            self._columns_data[el]["col_idx"]: el
+            for el in self._columns_data
+            if self._columns_data[el]["mask_idx"] is None
+               and self._columns_data[el]["col_idx"] is not None
         }
         self._ordinal_idx_list = list(self._ordinal_idx.keys())
 
     def fit(self, df: pd.DataFrame) -> None:
         self._check_df(df)
-        self.initial_columns_order = df.columns.tolist()
+        self._initial_columns_order = df.columns.tolist()
 
         new_df = OrderedDict()
         i = 0
 
         for el in df.columns:
-            self.columns_data[el] = {
+            self._columns_data[el] = {
                 "dtype": df[el].dtype,
                 "apply_round": False,
                 "col_idx": None,  # Columns id
@@ -70,43 +73,49 @@ class DataTransformer:
                 if mask.any():
                     new_df[el] = df[el].fillna(0, inplace=False)
                     new_df[f"{el}_mask"] = mask
-                    self.columns_data[el]["col_idx"] = i
-                    self.columns_data[el]["mask_idx"] = i + 1
+                    self._columns_data[el]["col_idx"] = i
+                    self._columns_data[el]["mask_idx"] = i + 1
                     i += 2
                 else:
                     new_df[el] = df[el]
-                    self.columns_data[el]["col_idx"] = i
+                    self._columns_data[el]["col_idx"] = i
                     i += 1
 
                 if (df[el][~mask].round() == df[el][~mask]).fillna(False).all():
-                    self.columns_data[el]["apply_round"] = True
+                    self._columns_data[el]["apply_round"] = True
 
             else:
                 cat_df = pd.get_dummies(df[el], dummy_na=True)
-                self.columns_data[el]["one_hot"]["categories"] = cat_df.columns.to_list()
-                self.columns_data[el]["one_hot"]["one_hot_idx"] = [i, i + cat_df.shape[1] - 1]
+                cat_df.columns = cat_df.columns.fillna(MISSING_VALUE)
+                self._columns_data[el]["one_hot"]["categories"] = cat_df.columns.to_list()
+                self._columns_data[el]["one_hot"]["one_hot_idx"] = [i, i + cat_df.shape[1] - 1]
                 i += cat_df.shape[1]
 
                 for col in cat_df.columns:
                     new_df[f"{el}_{col}"] = cat_df[col]
 
-        self.final_scaler.fit(pd.DataFrame(new_df).to_numpy())
+        self._final_scaler.fit(pd.DataFrame(new_df).to_numpy())
 
-        self.final_size = len(new_df)
+        self._final_size = len(new_df)
         self._prepare_indices()
 
-        self.fitted = True
+        self._fitted = True
 
     def transform(self, df: pd.DataFrame) -> torch.Tensor:
+        if not self._fitted:
+            raise RuntimeError(
+                "fit method must be called before transform"
+            )
+
         self._check_df(df)
 
         new_df = OrderedDict()
 
-        for el in self.initial_columns_order:
-            if self.columns_data[el]["col_idx"] is not None:
+        for el in self._initial_columns_order:
+            if self._columns_data[el]["col_idx"] is not None:
                 new_df[el] = df[el]
 
-                if self.columns_data[el]["mask_idx"] is not None:
+                if self._columns_data[el]["mask_idx"] is not None:
                     new_df[el] = df[el].fillna(0, inplace=False)
                     new_df[f"{el}_mask"] = df[el].isna()
                 else:
@@ -114,21 +123,22 @@ class DataTransformer:
 
             else:
                 cat_df = pd.get_dummies(df[el], dummy_na=True)
+                cat_df.columns = cat_df.columns.fillna(MISSING_VALUE)
                 cat_df_cols_set = set(cat_df.columns)
 
-                extra = cat_df_cols_set - set(self.columns_data[el]["one_hot"]["categories"])
+                extra = cat_df_cols_set - set(self._columns_data[el]["one_hot"]["categories"])
 
                 if extra:
                     raise ValueError(f"Unexpected categories: {extra}")
 
-                for col in self.columns_data[el]["one_hot"]["categories"]:
+                for col in self._columns_data[el]["one_hot"]["categories"]:
                     if col not in cat_df_cols_set:
                         new_df[f"{el}_{col}"] = [False] * df.shape[0]
                     else:
                         new_df[f"{el}_{col}"] = cat_df[col]
 
         return torch.tensor(
-            self.final_scaler.transform(
+            self._final_scaler.transform(
                 pd.DataFrame(new_df).to_numpy()
             ),
             dtype=torch.float32,
@@ -176,41 +186,77 @@ class DataTransformer:
         return loss
 
     def inverse_transform(self, tensor: torch.Tensor) -> pd.DataFrame:
-        if not self.fitted:
+        if not self._fitted:
             raise RuntimeError(
                 "fit method must be called before inverse_transform"
             )
 
-        tensor_unscaled = self.final_scaler.inverse_transform(tensor)
+        tensor_unscaled = self._final_scaler.inverse_transform(tensor)
         new_df = {value: tensor_unscaled[:, key] for key, value in self._ordinal_idx.items()}
 
         for key, value in self._softmax_idx.items():
             new_df[value] = [
-                self.columns_data["one_hot"]["categories"][i]
-                for i in np.argmax(tensor_unscaled[:, key[0]: key[1] + 1].numpy(), axis=1)
+                self._columns_data[value]["one_hot"]["categories"][i]
+                if self._columns_data[value]["one_hot"]["categories"][i] != MISSING_VALUE
+                else np.nan
+
+                for i in np.argmax(tensor_unscaled[:, key[0]: key[1] + 1], axis=1)
             ]
 
         for key, value in self._masked_idx.items():
-            ser_tens = pd.Series(tensor_unscaled[:, key[0]].numpy())
-            mask = tensor_unscaled[:, key[1]].numpy() >= 0.5
+            ser_tens = pd.Series(tensor_unscaled[:, key[0]])
+            mask = tensor_unscaled[:, key[1]] >= 0.5
             ser_tens[mask] = np.nan
             new_df[value] = ser_tens
 
         df = pd.DataFrame(new_df)
-        for el in self.columns_data:
+        for el in self._columns_data:
 
-            if self.columns_data[el]["apply_round"]:
+            if self._columns_data[el]["apply_round"]:
                 df[el] = df[el].round()
 
-            df[el] = df[el].astype(self.columns_data[el]["dtype"])
+            df[el] = df[el].astype(self._columns_data[el]["dtype"])
 
-
-        return df
-
+        return df.reindex(columns=self._initial_columns_order)
 
     def get_params(self) -> dict:
-        pass
+        if not self._fitted:
+            raise RuntimeError(
+                "fit method must be called before get_params"
+            )
+
+        data = {
+            "columns_data": dict(self._columns_data),
+            "initial_columns_order": list(self._initial_columns_order),
+            "final_size": self._final_size,
+            "final_scaler": {
+                "scale_": self._final_scaler.scale_.tolist(),
+                "min_": self._final_scaler.min_.tolist(),
+                "data_min_": self._final_scaler.data_min_.tolist(),
+                "data_max_": self._final_scaler.data_max_.tolist(),
+                "feature_range": self._final_scaler.feature_range,
+            },
+        }
+        return data
 
     @classmethod
     def save_params(cls, params: dict) -> "DataTransformer":
-        pass
+        self = cls.__new__(cls)
+
+        self._columns_data = params["columns_data"]
+        self._initial_columns_order = params["initial_columns_order"]
+        self._final_size = params["final_size"]
+
+        fs = params["final_scaler"]
+        scaler = MinMaxScaler(feature_range=tuple(fs["feature_range"]))
+        scaler.scale_ = np.array(fs["scale_"])
+        scaler.min_ = np.array(fs["min_"])
+        scaler.data_min_ = np.array(fs["data_min_"])
+        scaler.data_max_ = np.array(fs["data_max_"])
+        scaler.data_range_ = scaler.data_max_ - scaler.data_min_
+        self._final_scaler = scaler
+
+        self._fitted = True
+        self._prepare_indices()
+
+        return self
--- oversampling/tests/DataTransformerTest.py
+++ oversampling/tests/DataTransformerTest.py
@@ -22,14 +22,14 @@ def test_not_fitted_call1():
     with pytest.raises(RuntimeError) as e:
         transformer = DataTransformer().inverse_transform(torch.Tensor([[1, 1]]))
     assert (
-        str(e.value) == "fit_transform method must be called before inverse_transform"
+        str(e.value) == "fit method must be called before inverse_transform"
     )
 
 
 def test_not_fitted_call2():
     with pytest.raises(RuntimeError) as e:
         transformer = DataTransformer().get_params()
-    assert str(e.value) == "fit_transform method must be called before get_params"
+    assert str(e.value) == "fit method must be called before get_params"
 
 
 def test_output_type():
@@ -38,15 +38,6 @@ def test_output_type():
 
     assert isinstance(transformed_tensor, torch.Tensor)
 
-
-def test_output_shape():
-    transformer = DataTransformer()
-    transformed_tensor = transformer.fit_transform(test_data)
-
-    assert tuple(transformed_tensor.shape) == (5, 6)
-    # 5 initial + 1 mask (in cat cols NaN encoded as category)
-    # masks for NaN-s are used only for numeric and bool columns "binary" is an obj columns
-    # as it contains NaN-s
 
 
 def test_inverse_transform():
@@ -63,22 +54,6 @@ def test_inverse_transform():
     )
 
 
-def test_data_transformer_serialization_structure():
-    transformer = DataTransformer()
-    transformer.fit_transform(test_data)
-    state = transformer.get_params()
-
-    assert "final_scaler" in state
-    assert "encoders" in state
-    assert "min_max" in state
-    assert "types" in state
-    assert "columns" in state
-    assert "apply_round" in state
-
-    json_state = json.dumps(state, indent=2)
-    assert isinstance(json_state, str)
-
-
 def test_data_transformer_serialization_load():
     transformer = DataTransformer()
     transformer.fit_transform(test_data)
@@ -86,16 +61,12 @@ def test_data_transformer_serialization_load():
     state = transformer.get_params()
     loaded_transformer = DataTransformer.save_params(state)
 
-    original_scaler = transformer.final_scaler
-    loaded_scaler = loaded_transformer.final_scaler
+    original_scaler = transformer._final_scaler
+    loaded_scaler = loaded_transformer._final_scaler
 
     assert np.allclose(original_scaler.scale_, loaded_scaler.scale_)
     assert np.allclose(original_scaler.min_, loaded_scaler.min_)
 
-    for col in transformer.encoders:
-        orig_cats = transformer.encoders[col].categories_[0]
-        loaded_cats = loaded_transformer.encoders[col].categories_[0]
-        assert np.array_equal(orig_cats, loaded_cats)
 
 
 def test_data_transformer_serialization_load_inverse():
@@ -168,21 +139,3 @@ def test_int_cols_with_na():
         check_dtype=True,
         check_exact=False,
     )
-
-
-def test_nan_order_cat_cols():
-    # The test is written for the falling case
-    # when we pass elments to OrdinalEncoder
-    # in such an order that nan is not at the end
-    # like that:
-    # self.encoders[el] = OrdinalEncoder(
-    #    categories=[df[el].unique()],
-    #    encoded_missing_value=-1
-    # )
-    # it might give such error E
-
-    # ValueError: Nan should be the last element in user provided categories,
-    # see categories [nan 'a'] in column #0
-    data = pd.DataFrame({"1": [np.nan, np.nan, np.nan, "a"]})
-    transformer = DataTransformer()
-    transformed = transformer.fit_transform(data)
